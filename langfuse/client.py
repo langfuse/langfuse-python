e0a1cdc25f5f7da2a79b3664336d5cb8292738d4
Max Deichmann <m.deichmann@tum.de>
2023-07-18 16:00:23 +0200

    make stateful client work

--- langfuse/client.py
+++ langfuse/client.py
@@ -4,6 +4,7 @@ from typing import Optional
 import uuid
 from langfuse import version
 from langfuse.api.resources.generations.types.create_log import CreateLog
+from langfuse.api.resources.span.types.create_span_request import CreateSpanRequest
 from langfuse.concurrency import FuturesStore
 from langfuse.api.client import AsyncFintoLangfuse
 from langfuse.api.resources.trace.types.create_trace_request import CreateTraceRequest
@@ -33,6 +34,14 @@ class Langfuse:
         self.future_store.append(future_id, trace_promise)
 
         return StatefulClient(self.client, State(StateType.TRACE, future_id), future_store=self.future_store)
+
+    def generation(self, body: CreateLog):
+            
+        generation_promise = lambda: self.client.generations.log(request=body)
+        future_id = uuid.uuid4()
+        self.future_store.append(future_id, generation_promise)
+
+        return StatefulClient(self.client, State(StateType.OBSERVATION, future_id), future_store=self.future_store)
         
 
     def flush(self):
@@ -55,6 +64,7 @@ class StatefulClient:
         self.client = client
         self.state = state
         self.future_store = future_store
+
 
     def generation(self, body: CreateLog):
         print('state', self.state.type, self.state.id)
@@ -82,3 +92,28 @@ class StatefulClient:
 
         return StatefulClient(self.client, State(StateType.OBSERVATION, id), future_store=self.future_store)
 
+    def span(self, body: CreateSpanRequest):
+        print('state', self.state.type, self.state.id)
+        print('generation', list(self.future_store.futures.keys()))
+        
+        id = uuid.uuid4() if body.id is None else body.id
+        print(self.future_store)
+        async def task(future_result):
+            new_body = body.copy(update={'id': id})
+
+            parent = future_result
+            
+            if self.state.type == StateType.OBSERVATION:
+                new_body = new_body.copy(update={'parent_observation_id': body.parent_observation_id if body.parent_observation_id is not None else parent.id})
+                new_body = new_body.copy(update={'trace_id': body.trace_id if body.trace_id is not None else parent.trace_id})
+            else:   
+                new_body = new_body.copy(update={'trace_id': body.trace_id if body.trace_id is not None else parent.id})
+            
+            print('parent', parent.dict())
+            print('new',new_body.dict())
+            return await self.client.span.create(request=new_body)
+
+        # Add the task to the future store with trace_future_id as a dependency
+        self.future_store.append(id, task, future_id=self.state.id)
+
+        return StatefulClient(self.client, State(StateType.OBSERVATION, id), future_store=self.future_store)
--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -3,6 +3,7 @@ import pytest
 
 from langfuse import Langfuse
 from langfuse.api.resources.generations.types.create_log import CreateLog
+from langfuse.api.resources.span.types.create_span_request import CreateSpanRequest
 from langfuse.api.resources.trace.types.create_trace_request import CreateTraceRequest
 
 @pytest.mark.timeout(5)
@@ -17,7 +18,19 @@ def test_create_trace():
 
     generation = trace.generation(CreateLog(name="his-is-so-great-new", metadata="test"))
 
-    generation.generation(CreateLog(name="yet another child", metadata="test"))
+    sub_generation = generation.generation(CreateLog(name="yet another child", metadata="test"))
+
+    sub_sub_span = sub_generation.span(CreateSpanRequest(name="sub-sub-span", metadata="test"))
+
+    client.flush()
+
+
+@pytest.mark.timeout(5)
+def test_create_trace():
+    client = Langfuse("pk-lf-1234567890","sk-lf-1234567890", 'http://localhost:3000')
+
+    generation = client.generation(CreateLog(name="top-level-generation", metadata="test"))
+    sub_generation = generation.generation(CreateLog(name="su-child", metadata="test"))
 
     client.flush()
 
