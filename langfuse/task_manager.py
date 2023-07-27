15f9fb3e494ad6c3dcad7c486744b70c46377523
Max Deichmann <m.deichmann@tum.de>
2023-07-27 15:58:37 +0200

    working

--- langfuse/task_manager.py
+++ langfuse/task_manager.py
@@ -14,92 +14,80 @@ class TaskManager:
         self.tasks = asyncio.Queue()
         self.result_mapping = {}
         self.processing = True
+        self.loop = asyncio.get_running_loop()
         self.consumer = asyncio.create_task(self.process_tasks())
         self.lock = asyncio.Lock()  # Lock for synchronizing add_task and wait_for_completion
+        self.result_mapping_lock = asyncio.Lock()  # Lock to sync  # Lock to synchronize result mapping
 
     async def process_tasks(self):
-        logging.info("Prcessing tasks...")
-        while True:
+        logging.info("Processing tasks...")
+        while self.processing:
+            task = await self.tasks.get()
+            logging.info(f"Processing task {task.id} with dependency {task.prev_id}...")
+
+            # Wait for the task's dependency to complete
+            if task.prev_id is not None:
+                prev_task_future = self.result_mapping.get(task.prev_id)
+                if prev_task_future is not None:
+                    await prev_task_future  # Suspend until the dependency is completed
+                prev_result = prev_task_future.result() if prev_task_future is not None else None
+            else:
+                prev_result = None
+
+            logging.info(f"Task {task.id} dependency completed with result {prev_result}")
             try:
-                task = await self.tasks.get()
-                logging.info(f"Processing task {task.id} with dependency {task.prev_id}...")
+                result = await task.coro(prev_result)
+                task_future = self.loop.create_future()
+                task_future.set_result(result)
+                self.result_mapping[task.id] = task_future  # Map task id to its future
+                logging.info(f"Task {task.id} completed successfully with result {result}")
+            except Exception as e:
+                task_future = self.loop.create_future()
+                task_future.set_exception(e)
+                self.result_mapping[task.id] = task_future  # Map task id to its future even in case of error
+                logging.error(f"Task {task.id} failed with exception {e}")
+                self.processing = False  # Stop processing tasks after the first failure
 
-                # Wait for the task's dependency to complete
-                if task.prev_id is not None:
-                    while task.prev_id not in self.result_mapping:
-                        await asyncio.sleep(0)  # Give control back to the event loop
-
-                prev_result = self.result_mapping.get(task.prev_id)[1] if task.prev_id else None
-                logging.info(f"Task {task.id} dependency completed with result {prev_result}")
-                try:
-                    result = await task.coro(prev_result)
-                    self.result_mapping[task.id] = (True, result)
-                    logging.info(f"Dropping task {task.prev_id} from result mapping")
-                    # self.result_mapping.pop(task.prev_id, None)
-                    logging.info(f"Task {task.id} completed successfully with result {result}")
-                except Exception as e:
-                    self.result_mapping[task.id] = (False, e)  # Task failed, store exception
-                    # self.clear_task_queue()  # Clear the task queue after a failure
-                    logging.error(f"Task {task.id} failed with exception {e}")
-                    break  # Stop processing tasks after the first failure
-
-                self.tasks.task_done()
-            except asyncio.CancelledError:
-                logging.info("Task processing was cancelled")
-                break  # Break the loop when cancelled
-            finally:
-                logging.info(f"Finally, {self.tasks.empty()}")
-                if self.tasks.empty():
-                    logging.info("No more tasks to process")
-                    self.processing = False
-                    break  # Break the loop when no more tasks
+            self.tasks.task_done()
+            if self.tasks.empty():
+                logging.info("No more tasks to process")
+                self.processing = False
 
     async def add_task(self, task):
-        async with self.lock:  # Acquire the lock before adding task
-            try:
-                logging.info(f"Adding task {task.id} with dependency {task.prev_id}...")
-                if not self.processing:
-                    logging.info("Starting task processing...")
-                    self.consumer = asyncio.create_task(self.process_tasks())
-                await self.tasks.put(task)
-            except Exception as e:
-                logging.error(f"Failed to add task {task.id} with exception {e}")
-
-    def clear_task_queue(self):
-        while not self.tasks.empty():
-            self.tasks.get_nowait()
+        async with self.lock:
+            logging.info(f"Adding task {task.id} with dependency {task.prev_id}...")
+            if not self.processing:
+                logging.info("Restarting task processing...")
+                self.processing = True
+                self.consumer = asyncio.create_task(self.process_tasks())
+            await self.tasks.put(task)
 
     async def wait_for_completion(self):
-        async with self.lock:  # Acquire the lock before waiting for completion
-            try:
-                logging.info("Waiting for task processing to complete...")
-                while self.processing:
-                    await asyncio.sleep(0)
-
-                for task_id, task in self.result_mapping.items():
-                    if not task[0]:
-                        return {"status": "fail", "failure_reason": task[1]}
-
-                if self.consumer:
-                    self.consumer.cancel()
-                    try:
-                        await self.consumer
-                    except asyncio.CancelledError:
-                        logging.info("Consumer task cancelled successfully")
-                self.consumer = None
-
-                logging.info(f"task manager result mapping {self.result_mapping.keys()}, {self.tasks.qsize()}")
-                return {"status": "success"}
-            except Exception as e:
-                logging.error(f"Failed to wait for task completion with exception {e}")
-                return {"status": "fail", "failure_reason": e}
+        async with self.lock:
+            logging.info("Waiting for task processing to complete...")
+            while self.processing:
+                await asyncio.sleep(0)  # Give control back to the event loop
+            if self.consumer:
+                self.consumer.cancel()
+                try:
+                    await self.consumer
+                except asyncio.CancelledError:
+                    logging.info("Consumer task cancelled successfully")
+            self.consumer = None
+            for task_id, task_future in self.result_mapping.items():
+                try:
+                    _ = task_future.result()  # Check if the task was successful
+                except Exception as e:  # The task failed
+                    logging.error(f"Task {task_id} failed with exception {e}")
+                    return {"status": "fail", "failure_reason": str(e)}
+            logging.info(f"Task manager result mapping {self.result_mapping.keys()}, {self.tasks.qsize()}")
+            return {"status": "success"}
 
     def __del__(self):
-        # Cleanup consumer when the TaskManager instance is destroyed
         if self.consumer is not None:
             self.consumer.cancel()
             try:
-                asyncio.get_event_loop().run_until_complete(self.consumer)
+                self.loop.run_until_complete(self.consumer)
             except asyncio.CancelledError:
                 pass
             self.consumer = None
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -20,3 +20,27 @@ async def test_task_manager():
     await tm.add_task(Task(4, lambda x: my_task(x, 4), prev_id=3))
 
     await tm.wait_for_completion()
+
+
+@pytest.mark.asyncio
+async def test_task_manager_fail():
+    async def my_task(input, multiplier):
+        await asyncio.sleep(1)  # simulates IO-bound task
+        return (input or 1) * multiplier
+
+    async def my_failing_task(input, multiplier):
+        await asyncio.sleep(1)  # simulates IO-bound task
+        raise Exception("This task failed")
+
+    tm = TaskManager()
+
+    # Add tasks
+    await tm.add_task(Task(1, lambda x: my_task(x, 2)))
+    await tm.add_task(Task(2, lambda x: my_task(x, 3), prev_id=1))
+    await tm.add_task(Task(3, lambda x: my_failing_task(x, 4), prev_id=2))
+
+    await tm.add_task(Task(4, lambda x: my_task(x, 4), prev_id=3))
+
+    result = await tm.wait_for_completion()
+
+    assert result["status"] == "fail"
