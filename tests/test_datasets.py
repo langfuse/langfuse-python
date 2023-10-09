cd32eb4656704f76cde379850132680415a3c204
Marc Klingen <git@marcklingen.com>
2023-10-09 20:08:57 +0200

    Allow linking observation to dataset run via id (#120)

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -65,6 +65,35 @@ def test_linking_observation():
     assert run.dataset_run_items[0].observation_id == generation_id
 
 
+def test_linking_via_id_observation():
+    langfuse = Langfuse(debug=True)
+
+    dataset_name = create_uuid()
+    langfuse.create_dataset(CreateDatasetRequest(name=dataset_name))
+
+    input = json.dumps({"input": "Hello World"})
+    langfuse.create_dataset_item(CreateDatasetItemRequest(dataset_name=dataset_name, input=input))
+
+    dataset = langfuse.get_dataset(dataset_name)
+    assert len(dataset.items) == 1
+    assert dataset.items[0].input == input
+
+    run_name = create_uuid()
+    generation_id = create_uuid()
+
+    for item in dataset.items:
+        langfuse.generation(InitialGeneration(id=generation_id))
+        langfuse.flush()
+
+        item.link(generation_id, run_name)
+
+    run = langfuse.get_dataset_run(dataset_name, run_name)
+
+    assert run.name == run_name
+    assert len(run.dataset_run_items) == 1
+    assert run.dataset_run_items[0].observation_id == generation_id
+
+
 @pytest.mark.skip(reason="inference cost")
 def test_langchain_dataset():
     langfuse = Langfuse(debug=True)
