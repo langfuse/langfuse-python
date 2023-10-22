f3d2877acfe8f2aebe23b2489f9fdc18775c1dbc
Max Deichmann <m.deichmann@tum.de>
2023-10-22 22:42:41 +0200

    additional test

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -19,9 +19,9 @@ api = FintoLangfuse(
 
 # @pytest.mark.skip(reason="inference cost")
 def test_openai_chat_completion():
-    trace_id = create_uuid()
+    generation_name = create_uuid()
     completion = openai.ChatCompletion.create(
-        name=trace_id,
+        name=generation_name,
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": "1 + 1 = "}],
         temperature=0,
@@ -30,10 +30,10 @@ def test_openai_chat_completion():
 
     openai.flush_langfuse()
 
-    generation = api.observations.get_many(name=trace_id, type="GENERATION")
+    generation = api.observations.get_many(name=generation_name, type="GENERATION")
 
     assert len(generation.data) != 0
-    assert generation.data[0].name == trace_id
+    assert generation.data[0].name == generation_name
     assert generation.data[0].metadata == {"someKey": "someResponse"}
     assert len(completion.choices) != 0
     assert completion.choices[0].message.content == generation.data[0].output
@@ -55,11 +55,50 @@ def test_openai_chat_completion():
     assert generation.data[0].total_tokens is not None
 
 
+def test_openai_chat_completion_two_calls():
+    generation_name = create_uuid()
+    completion = openai.ChatCompletion.create(
+        name=generation_name,
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "1 + 1 = "}],
+        temperature=0,
+        metadata={"someKey": "someResponse"},
+    )
+
+    generation_name_2 = create_uuid()
+
+    completion_2 = openai.ChatCompletion.create(
+        name=generation_name_2,
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "2 + 2 = "}],
+        temperature=0,
+        metadata={"someKey": "someResponse"},
+    )
+
+    openai.flush_langfuse()
+
+    generation = api.observations.get_many(name=generation_name, type="GENERATION")
+
+    assert len(generation.data) != 0
+    assert generation.data[0].name == generation_name
+    assert len(completion.choices) != 0
+    assert completion.choices[0].message.content == generation.data[0].output
+    assert generation.data[0].input == "1 + 1 = "
+
+    generation_2 = api.observations.get_many(name=generation_name_2, type="GENERATION")
+
+    assert len(generation_2.data) != 0
+    assert generation_2.data[0].name == generation_name_2
+    assert len(completion_2.choices) != 0
+    assert completion_2.choices[0].message.content == generation_2.data[0].output
+    assert generation_2.data[0].input == "2 + 2 = "
+
+
 # @pytest.mark.skip(reason="inference cost")
 def test_openai_completion():
-    trace_id = create_uuid()
+    generation_name = create_uuid()
     completion = openai.Completion.create(
-        name=trace_id,
+        name=generation_name,
         model="gpt-3.5-turbo-instruct",
         prompt="1 + 1 = ",
         temperature=0,
@@ -68,10 +107,10 @@ def test_openai_completion():
 
     openai.flush_langfuse()
 
-    generation = api.observations.get_many(name=trace_id, type="GENERATION")
+    generation = api.observations.get_many(name=generation_name, type="GENERATION")
 
     assert len(generation.data) != 0
-    assert generation.data[0].name == trace_id
+    assert generation.data[0].name == generation_name
     assert generation.data[0].metadata == {"someKey": "someResponse"}
     assert len(completion.choices) != 0
     assert completion.choices[0].text == generation.data[0].output
