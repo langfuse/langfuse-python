13c707e4b9ccc52ff285bb2f2a238302ca0632b8
Max Deichmann <m.deichmann@tum.de>
2023-10-23 23:04:58 +0200

    fix-openai-no-values

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -54,6 +54,17 @@ def test_openai_chat_completion():
     assert generation.data[0].total_tokens is not None
 
 
+def test_openai_chat_completion_without_extra_param():
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": "1 + 1 = "}],
+        temperature=0,
+        metadata={"someKey": "someResponse"},
+    )
+
+    assert len(completion.choices) != 0
+
+
 def test_openai_chat_completion_two_calls():
     generation_name = create_uuid()
     completion = openai.ChatCompletion.create(
