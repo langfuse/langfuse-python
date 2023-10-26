73f61a8af80df6fff604a257afb260249787c2a2
Max Deichmann <m.deichmann@tum.de>
2023-10-26 15:43:35 +0200

    adjustable amount of threads (#140)

--- langfuse/task_manager.py
+++ langfuse/task_manager.py
@@ -1,9 +1,9 @@
 import atexit
-from datetime import datetime, timedelta
-from enum import Enum
 import logging
 import queue
+from queue import Queue
 import threading
+from typing import List
 
 import backoff
 
@@ -11,31 +11,74 @@ from langfuse.logging import clean_logger
 
 
 class Task:
-    def __init__(self, task_id, function, predecessor_id: str = None):
+    def __init__(self, task_id, function):
         self.task_id = task_id
-        self.predecessor_id = predecessor_id
         self.function = function
-        self.result = None
-        self.timestamp = None
-        self.lock = threading.Lock()
-        self.status = TaskStatus.UNSCHEDULED
 
 
-class TaskStatus(Enum):
-    SUCCESS = "success"
-    FAIL = "fail"
-    UNSCHEDULED = "unscheduled"
+class Consumer(threading.Thread):
+    log = logging.getLogger("langfuse")
+    queue: Queue
+    identifier: int
+
+    def __init__(self, queue, identifier):
+        """Create a consumer thread."""
+
+        threading.Thread.__init__(self)
+        # Make consumer a daemon thread so that it doesn't block program exit
+        self.daemon = True
+        self.queue = queue
+        # It's important to set running in the constructor: if we are asked to
+        # pause immediately after construction, we might set running to True in
+        # run() *after* we set it to False in pause... and keep running
+        # forever.
+        self.running = True
+        self.identifier = identifier
+
+    def run(self):
+        """Runs the consumer."""
+        while self.running:
+            try:
+                task = self.queue.get(block=True, timeout=1)
+
+                self.log.debug(f"Task {task.task_id} received from the queue")
+
+                self._execute_task(task)
+                self.log.debug(f"Task {task.task_id} done")
+                self.queue.task_done()
+
+            except queue.Empty:
+                pass
+
+    def pause(self):
+        """Pause the consumer."""
+        self.running = False
+
+    def _execute_task(self, task: Task):
+        try:
+            self.log.debug(f"Task {task.task_id} executing")
+            result = self._execute_task_with_backoff(task)
+            self.log.debug(f"Task {task.task_id} done with result {result}")
+        except Exception as e:
+            self.log.warning(f"Task {task.task_id} failed with exception {e} ")
+
+    @backoff.on_exception(backoff.expo, Exception, max_tries=3)
+    def _execute_task_with_backoff(self, task: Task):
+        return task.function()
 
 
 class TaskManager(object):
     log = logging.getLogger("langfuse")
+    consumers: List[Consumer]
+    number_of_consumers: int
+    max_task_queue_size: int
+    queue: Queue
 
-    def __init__(self, debug=False, max_task_queue_size=10_000, max_task_age=600):
+    def __init__(self, debug=False, max_task_queue_size=10_000, number_of_consumers=1):
         self.max_task_queue_size = max_task_queue_size
+        self.number_of_consumers = number_of_consumers
         self.queue = queue.Queue(max_task_queue_size)
-        self.consumer_thread = None
-        self.result_mapping = {}
-        self.max_task_age = max_task_age
+        self.consumers = []
         if debug:
             # Ensures that debug level messages are logged when debug mode is on.
             # Otherwise, defaults to WARNING level.
@@ -53,16 +96,15 @@ class TaskManager(object):
         atexit.register(self.join)
 
     def init_resources(self):
-        self.consumer_thread = Consumer(self.queue, self.result_mapping, self.max_task_age)
-        self.consumer_thread.start()
+        for i in range(self.number_of_consumers):
+            consumer = Consumer(self.queue, i)
+            consumer.start()
+            self.consumers.append(consumer)
 
-    def add_task(self, task_id, function, predecessor_id=None):
+    def add_task(self, task_id, function):
         try:
-            self.log.debug(f"Adding task {task_id} with predecessor {predecessor_id}")
-            if self.consumer_thread is None or not self.consumer_thread.is_alive():
-                self.init_resources()
-            task = Task(task_id, function, predecessor_id)
-
+            self.log.debug(f"Adding task {task_id}")
+            task = Task(task_id, function)
             self.queue.put(task, block=False)
             self.log.debug(f"Task {task_id} added to queue")
         except queue.Full:
@@ -82,17 +124,19 @@ class TaskManager(object):
         self.log.debug("successfully flushed about %s items.", size)
 
     def join(self):
-        """Ends the consumer thread once the queue is empty.
+        """Ends the consumer threads once the queue is empty.
         Blocks execution until finished
         """
-        self.log.debug("joining consumer thread")
-        self.consumer_thread.pause()
-        try:
-            self.consumer_thread.join()
-        except RuntimeError:
-            # consumer thread has not started
-            pass
-        self.log.debug("consumer thread joined")
+        self.log.debug(f"joining {len(self.consumers)} consumer threads")
+        for consumer in self.consumers:
+            consumer.pause()
+            try:
+                consumer.join()
+            except RuntimeError:
+                # consumer thread has not started
+                pass
+
+            self.log.debug(f"consumer thread {consumer.identifier} joined")
 
     def shutdown(self):
         """Flush all messages and cleanly shutdown the client"""
@@ -100,95 +144,3 @@ class TaskManager(object):
         self.flush()
         self.join()
         self.log.debug("shutdown completed")
-
-    def get_result(self, task_id):
-        try:
-            return self.result_mapping.get(task_id)
-        except Exception as e:
-            self.log.warning(f"Exception in getting result for task {task_id} {e}")
-
-
-class Consumer(threading.Thread):
-    log = logging.getLogger("langfuse")
-
-    def __init__(self, queue, result_mapping, max_task_age):
-        """Create a consumer thread."""
-
-        threading.Thread.__init__(self)
-        # Make consumer a daemon thread so that it doesn't block program exit
-        self.daemon = True
-        self.queue = queue
-        # It's important to set running in the constructor: if we are asked to
-        # pause immediately after construction, we might set running to True in
-        # run() *after* we set it to False in pause... and keep running
-        # forever.
-        self.running = True
-        self.result_mapping = result_mapping
-        self.max_task_age = max_task_age
-
-    def run(self):
-        """Runs the consumer."""
-        self.log.debug("consumer is running...")
-        while self.running:
-            try:
-                self.log.debug("consumer looping")
-                self._prune_old_tasks(self.max_task_age)
-
-                # elapsed = time.monotonic.monotonic() - start_time
-                task = self.queue.get(block=True, timeout=1)
-
-                self.result_mapping[task.task_id] = task
-
-                self.log.debug(f"Task {task.task_id} received from the queue")
-
-                self._execute_task(task)
-                self.log.debug(f"Task {task.task_id} done")
-                self.queue.task_done()
-
-            except queue.Empty:
-                break
-
-        self.log.debug("consumer exited.")
-
-    def pause(self):
-        """Pause the consumer."""
-        self.running = False
-
-    def _execute_task(self, task: Task):
-        try:
-            self.log.debug(f"Task {task.task_id} executing")
-            self._execute_task_with_backoff(task)
-        except Exception as e:
-            self.result_mapping[task.task_id].result = e
-            self.result_mapping[task.task_id].status = TaskStatus.FAIL
-            self.result_mapping[task.task_id].timestamp = datetime.now()
-            self.log.warning(f"Task {task.task_id} failed with exception {e} ")
-
-    @backoff.on_exception(backoff.expo, Exception, max_tries=3)
-    def _execute_task_with_backoff(self, task: Task):
-        result = None
-        self.log.debug(f"Task {task.task_id} executing with backoff")
-        with task.lock:
-            result = task.function()
-            self.result_mapping[task.task_id].result = result
-            self.result_mapping[task.task_id].status = TaskStatus.SUCCESS
-            self.result_mapping[task.task_id].timestamp = datetime.now()
-            self.log.debug(f"Task {task.task_id} done with result {result}")
-
-    def _prune_old_tasks(self, delta: int):
-        try:
-            self.log.debug("Pruning old tasks")
-            now = datetime.now()
-
-            to_remove = [
-                task_id
-                for task_id, task in self.result_mapping.items()
-                if task.status in [TaskStatus.SUCCESS, TaskStatus.FAIL]
-                and task.timestamp
-                and now - task.timestamp > timedelta(seconds=delta)
-            ]
-            for task_id in to_remove:
-                self.result_mapping.pop(task_id, None)
-                self.log.debug(f"Task {task_id} pruned due to age")
-        except Exception as e:
-            self.log.error(f"Exception in pruning old tasks {e}")
--- tests/test_task_manager.py
+++ tests/test_task_manager.py
@@ -4,13 +4,16 @@ import time
 
 import pytest
 
-from langfuse.task_manager import TaskManager, TaskStatus
+from langfuse.task_manager import TaskManager
 
 
 @pytest.mark.timeout(10)
 def test_multiple_tasks_without_predecessor():
+    counter = 0
+
     def task_without_predecessor():
-        return 20
+        nonlocal counter
+        counter = counter + 1
 
     tm = TaskManager()
 
@@ -19,51 +22,22 @@ def test_multiple_tasks_without_predecessor():
     tm.add_task(30, task_without_predecessor)
 
     tm.shutdown()
-
-    assert tm.get_result(10).result == 20
-    assert tm.get_result(20).result == 20
-    assert tm.get_result(30).result == 20
-
-
-@pytest.mark.timeout(10)
-def test_task_manager():
-    def my_task():
-        return 2
-
-    def my_other_task():
-        return 5
-
-    tm = TaskManager(debug=False)
-
-    # Add tasks
-    tm.add_task(1, my_task)
-    tm.add_task(2, my_task)
-    tm.add_task(3, my_task)
-    tm.add_task(10, my_other_task)
-    tm.add_task(11, my_other_task)
-    tm.add_task(12, my_other_task)
-
-    # Check the results of the tasks
-    tm.shutdown()
-
-    assert tm.get_result(1).result == 2
-    assert tm.get_result(2).result == 2
-    assert tm.get_result(3).result == 2
-    assert tm.get_result(10).result == 5
-    assert tm.get_result(11).result == 5
-    assert tm.get_result(12).result == 5
+    assert counter == 3
 
 
 @pytest.mark.timeout(10)
 def test_task_manager_fail():
     retry_count = 0
+    counter = 0
 
     def my_task():
+        nonlocal counter
+        counter = counter + 1
         time.sleep(1)
-        return 2
 
     def my_failing_task():
         nonlocal retry_count
+        nonlocal counter
         time.sleep(1)
         retry_count += 1
         raise Exception(f"This task failed {retry_count}")
@@ -72,58 +46,41 @@ def test_task_manager_fail():
 
     tm.add_task(1, my_task)
     tm.add_task(2, my_task)
-    tm.join()
+    tm.flush()
     tm.add_task(3, my_failing_task)
     tm.add_task(4, my_task)
 
-    tm.shutdown()
+    tm.flush()
 
-    assert tm.get_result(1).result == 2
-    assert tm.get_result(2).result == 2
-    assert tm.get_result(3).status == TaskStatus.FAIL
-    assert tm.get_result(4).status == TaskStatus.SUCCESS
-    assert tm.get_result(4).result == 2
+    assert counter == 3
     assert retry_count == 3
-
-
-# @pytest.mark.timeout(10)
-# def test_task_manager_prune():
-#     def first(prev_result):
-#         return 2
-
-#     def my_task(input):
-#         return (input or 1) * 2
-
-#     tm = TaskManager(max_task_age=0)
-
-#     # Add tasks
-#     tm.add_task(1, first)
-#     tm.add_task(2, my_task, predecessor_id=1)
-#     tm.join()
-
-#     assert True if tm.get_result(1) is None else False
-#     assert True if tm.get_result(2) is None else False
 
 
 @pytest.mark.timeout(20)
 def test_consumer_restart():
+    counter = 0
+
     def short_task():
-        return 2
+        nonlocal counter
+        counter = counter + 1
 
     tm = TaskManager(debug=False)
     tm.add_task(1, short_task)
-    tm.join()
+    tm.flush()
 
     tm.add_task(2, short_task)
     tm.shutdown()
 
-    assert tm.get_result(2).result == 2
+    assert counter == 2
 
 
 @pytest.mark.timeout(10)
 def test_concurrent_task_additions():
+    counter = 0
+
     def concurrent_task():
-        return 2
+        nonlocal counter
+        counter = counter + 1
 
     def add_task_concurrently(tm, task_id, func):
         tm.add_task(task_id, func)
@@ -137,8 +94,7 @@ def test_concurrent_task_additions():
 
     tm.shutdown()
 
-    for i in range(10):
-        assert tm.get_result(i + 1).result == 2
+    assert counter == 10
 
 
 @pytest.mark.timeout(10)
@@ -185,7 +141,7 @@ manager.add_task(2, dummy_function)
 
     print(process.stderr)
 
-    assert "consumer thread joined" in logs
+    assert "consumer thread 0 joined" in logs
 
 
 def test_flush():
@@ -209,7 +165,7 @@ def test_shutdown():
     def short_task():
         return 2
 
-    tm = TaskManager(debug=False)  # debug=False to avoid logging
+    tm = TaskManager(debug=False, number_of_consumers=5)  # debug=False to avoid logging
 
     for i in range(1000):
         tm.add_task(i, short_task)
@@ -219,4 +175,8 @@ def test_shutdown():
     # 1. client queue is empty
     # 2. consumer thread has stopped
     assert tm.queue.empty()
-    assert not tm.consumer_thread.is_alive()
+
+    assert len(tm.consumers) == 5
+    for c in tm.consumers:
+        assert not c.is_alive()
+    assert tm.queue.empty()
