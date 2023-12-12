7d92fde4dba6ee89d38e297a9cf1f8e6c5657760
Max Deichmann <m.deichmann@tum.de>
2023-12-12 18:13:31 +0100

    push

--- langfuse/serializer.py
+++ langfuse/serializer.py
@@ -1,15 +1,20 @@
 from datetime import date, datetime, timezone
 from typing import Any
 from json import JSONEncoder
-from langchain.load.serializable import Serializable
-
 from pydantic import BaseModel
+
+# Attempt to import Serializable
+try:
+    from langchain.load.serializable import Serializable
+except ImportError:
+    # If Serializable is not available, set it to NoneType
+    Serializable = type(None)
 
 
 class EventSerializer(JSONEncoder):
     def default(self, obj: Any):
         if isinstance(obj, (date, datetime)):
-            # make the datetime object timezone-aware if it isn't already
+            # Timezone-awareness check
             if obj.tzinfo is None or obj.tzinfo.utcoffset(obj) is None:
                 obj = obj.replace(tzinfo=timezone.utc)
             return obj.isoformat()
@@ -18,7 +23,7 @@ class EventSerializer(JSONEncoder):
         if isinstance(obj, Serializable):
             return obj.to_json()
 
-        # If the object is of a type that the JSON encoder can handle, return the original object
+        # Standard JSON-encodable types
         if isinstance(obj, (dict, list, str, int, float, type(None))):
             return obj
 
