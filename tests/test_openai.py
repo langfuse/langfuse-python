f38bcc06f24653adcbb6ea7fd1eb1bf2626bc9e7
Max Deichmann <m.deichmann@tum.de>
2024-02-16 11:13:53 +0100

    added test

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -107,6 +107,7 @@ def test_openai_chat_completion_stream():
     assert generation.data[0].usage.output is not None
     assert generation.data[0].usage.total is not None
     assert generation.data[0].output == "2"
+    assert isinstance(generation.data[0].output, str) is True
     assert generation.data[0].completion_start_time is not None
 
 
