97a712b9347d2c203cf4f9bf2c834fa98b629e39
Max Deichmann <m.deichmann@tum.de>
2024-02-28 23:01:25 +0100

    push

--- langfuse/callback/langchain.py
+++ langfuse/callback/langchain.py
@@ -5,7 +5,7 @@ log = logging.getLogger("langfuse")
 try:  # Test that langchain is installed before proceeding
     import langchain
 except ImportError as e:
-    log.exception(f"Could not import langchain. Some functionality may be missing. {e}")
+    log.error(f"Could not import langchain. The langchain integration will not work. {e}")
 
 from typing import Any, Dict, List, Optional, Sequence, Union
 from uuid import UUID, uuid4
