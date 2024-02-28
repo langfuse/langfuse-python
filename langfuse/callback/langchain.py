e01d0226ccdbce35a168b4f98218566654b46406
hubert-springbok <115166625+hubert-springbok@users.noreply.github.com>
2024-02-28 22:23:42 +0000

    Cleanup and explicit type hints (#405)

--- langfuse/callback/langchain.py
+++ langfuse/callback/langchain.py
@@ -1,4 +1,14 @@
 import logging
+
+log = logging.getLogger("langfuse")
+
+try:  # Test that langchain is installed before proceeding
+    import langchain
+except ImportError as e:
+    log.error(
+        f"Could not import langchain. The langchain integration will not work. {e}"
+    )
+
 from typing import Any, Dict, List, Optional, Sequence, Union
 from uuid import UUID, uuid4
 
