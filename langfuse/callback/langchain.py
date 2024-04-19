ee1182ee8b2050c53d210563d7d892494d5b9a85
jiamu <jiamu@jd.com>
2024-04-19 16:47:02 +0800

    Fix span input empty when call langchain stream

--- langfuse/callback/langchain.py
+++ langfuse/callback/langchain.py
@@ -160,6 +160,7 @@ class LangchainCallbackHandler(
                 level=ObservationLevel.ERROR,
                 status_message=str(error),
                 version=self.version,
+                input=kwargs.get("inputs"),
             )
         except Exception as e:
             self.log.exception(e)
@@ -283,7 +284,9 @@ class LangchainCallbackHandler(
                 raise Exception("run not found")
 
             self.runs[run_id] = self.runs[run_id].end(
-                output=action, version=self.version
+                output=action,
+                version=self.version,
+                input=kwargs.get("inputs"),
             )
 
         except Exception as e:
@@ -305,7 +308,9 @@ class LangchainCallbackHandler(
                 raise Exception("run not found")
 
             self.runs[run_id] = self.runs[run_id].end(
-                output=finish, version=self.version
+                output=finish,
+                version=self.version,
+                input=kwargs.get("inputs"),
             )
 
             # langchain sends same run_id for agent_finish and chain_end for the same agent interaction.
@@ -334,7 +339,9 @@ class LangchainCallbackHandler(
                 raise Exception("run not found")
 
             self.runs[run_id] = self.runs[run_id].end(
-                output=outputs, version=self.version
+                output=outputs,
+                version=self.version,
+                input=kwargs.get("inputs"),
             )
             self._update_trace_and_remove_state(
                 run_id, parent_run_id, outputs, input=kwargs.get("inputs")
@@ -357,6 +364,7 @@ class LangchainCallbackHandler(
                 level=ObservationLevel.ERROR,
                 status_message=str(error),
                 version=self.version,
+                input=kwargs.get("inputs"),
             )
 
             self._update_trace_and_remove_state(
@@ -505,7 +513,9 @@ class LangchainCallbackHandler(
                 raise Exception("run not found")
 
             self.runs[run_id] = self.runs[run_id].end(
-                output=documents, version=self.version
+                output=documents,
+                version=self.version,
+                input=kwargs.get("inputs"),
             )
 
             self._update_trace_and_remove_state(run_id, parent_run_id, documents)
@@ -527,7 +537,9 @@ class LangchainCallbackHandler(
                 raise Exception("run not found")
 
             self.runs[run_id] = self.runs[run_id].end(
-                output=output, version=self.version
+                output=output,
+                version=self.version,
+                input=kwargs.get("inputs"),
             )
 
             self._update_trace_and_remove_state(run_id, parent_run_id, output)
@@ -552,6 +564,7 @@ class LangchainCallbackHandler(
                 status_message=str(error),
                 level=ObservationLevel.ERROR,
                 version=self.version,
+                input=kwargs.get("inputs"),
             )
 
             self._update_trace_and_remove_state(run_id, parent_run_id, error)
@@ -679,7 +692,10 @@ class LangchainCallbackHandler(
 
                 llm_usage = _parse_usage(response)
                 self.runs[run_id] = self.runs[run_id].end(
-                    output=extracted_response, usage=llm_usage, version=self.version
+                    output=extracted_response,
+                    usage=llm_usage,
+                    version=self.version,
+                    input=kwargs.get("inputs"),
                 )
 
                 self._update_trace_and_remove_state(
@@ -703,6 +719,7 @@ class LangchainCallbackHandler(
                 status_message=str(error),
                 level=ObservationLevel.ERROR,
                 version=self.version,
+                input=kwargs.get("inputs"),
             )
             self._update_trace_and_remove_state(run_id, parent_run_id, error)
 
