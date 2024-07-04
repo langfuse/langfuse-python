19e83b65ece8da7e5617e836f91e39e8cf4cc116
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2024-07-04 16:06:23 +0200

    fix test

--- tests/test_extract_model.py
+++ tests/test_extract_model.py
@@ -19,7 +19,6 @@ from langchain_community.chat_models import (
     ChatCohere,
     BedrockChat,
     ChatOllama,
-    ChatPerplexity,
 )
 
 from langchain_community.chat_models.fake import FakeMessagesListChatModel
@@ -177,15 +176,6 @@ def test_models(expected_model: str, model: Any):
             "gemini",
             ChatVertexAI(
                 model_name="gemini", credentials=MagicMock(), project="some-project"
-            ),
-        ),
-        (
-            "llama-3-8b-instruct",
-            ChatPerplexity(
-                model="llama-3-8b-instruct",
-                credentials=MagicMock(),
-                project="some-project",
-                pplx_api_key="pplx_api_key",
             ),
         ),
     ],
