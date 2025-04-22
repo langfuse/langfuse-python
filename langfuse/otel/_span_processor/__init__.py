1bef9c6176c061c35537e5fc986123c9506e96ea
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-04-22 13:10:33 +0200

    push

--- langfuse/otel/_span_processor/__init__.py
+++ langfuse/otel/_span_processor/__init__.py
@@ -24,13 +24,14 @@ class LangfuseSpanProcessor(BatchSpanProcessor):
     ):
         self.public_key = public_key
 
+        basic_auth_header = "Basic " + base64.b64encode(
+            f"{public_key}:{secret_key}".encode("utf-8")
+        ).decode("ascii")
+
         langfuse_span_exporter = OTLPSpanExporter(
             endpoint=f"{host}/api/public/otel/v1/traces",
             headers={
-                "Authorization": "Basic "
-                + base64.b64encode(f"{public_key}:{secret_key}".encode("utf-8")).decode(
-                    "ascii"
-                ),
+                "Authorization": basic_auth_header,
                 "x_langfuse_sdk_name": "python",
                 "x_langfuse_sdk_version": langfuse_version,
                 "x_langfuse_public_key": public_key,
