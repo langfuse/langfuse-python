20970e37317cc73c49ed3467b15f23583b02d5f6
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-05-05 16:51:06 +0200

    push

--- langfuse/otel/attributes.py
+++ langfuse/otel/attributes.py
@@ -20,7 +20,6 @@ class LangfuseSpanAttributes:
     TRACE_OUTPUT = "langfuse.trace.output"
 
     # Langfuse-observation attributes
-    OBSERVATION_NAME = "langfuse.observation.name"
     OBSERVATION_TYPE = "langfuse.observation.type"
     OBSERVATION_METADATA = "langfuse.observation.metadata"
     OBSERVATION_LEVEL = "langfuse.observation.level"
@@ -30,9 +29,9 @@ class LangfuseSpanAttributes:
 
     # Langfuse-observation of type Generation attributes
     OBSERVATION_COMPLETION_START_TIME = "langfuse.observation.completion_start_time"
-    OBSERVATION_MODEL = "gen_ai.response.model"
-    OBSERVATION_MODEL_PARAMETERS = "langfuse.observation.model_parameters"
-    OBSERVATION_USAGE_DETAILS = "gen_ai.usage"
+    OBSERVATION_MODEL = "langfuse.observation.model.name"
+    OBSERVATION_MODEL_PARAMETERS = "langfuse.observation.model.parameters"
+    OBSERVATION_USAGE_DETAILS = "langfuse.observation.usage_details"
     OBSERVATION_COST_DETAILS = "langfuse.observation.cost_details"
     OBSERVATION_PROMPT_NAME = "langfuse.observation.prompt.name"
     OBSERVATION_PROMPT_VERSION = "langfuse.observation.prompt.version"
@@ -115,7 +114,6 @@ def create_generation_attributes(
 ):
     attributes = {
         LangfuseSpanAttributes.OBSERVATION_TYPE: "generation",
-        LangfuseSpanAttributes.OBSERVATION_NAME: name,
         LangfuseSpanAttributes.OBSERVATION_LEVEL: level,
         LangfuseSpanAttributes.OBSERVATION_STATUS_MESSAGE: status_message,
         LangfuseSpanAttributes.VERSION: version,
