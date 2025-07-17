0ced31dc19ecbc601c122eb9bba9ca777b7ed9dd
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-07-17 16:36:27 +0200

    docs: update readme and reference

--- langfuse/__init__.py
+++ langfuse/__init__.py
@@ -1,7 +1,16 @@
 """.. include:: ../README.md"""
 
-from ._client.attributes import LangfuseOtelSpanAttributes  # noqa
 from ._client.client import Langfuse  # noqa
 from ._client.get_client import get_client  # noqa
 from ._client.observe import observe  # noqa
 from .version import __version__  # noqa
+from ._client.span import LangfuseSpan, LangfuseGeneration, LangfuseEvent
+
+__all__ = [
+    "Langfuse",
+    "get_client",
+    "observe",
+    "LangfuseSpan",
+    "LangfuseGeneration",
+    "LangfuseEvent",
+]
