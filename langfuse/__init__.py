8bd153336382bfab939f91f8bd573ec7ac246ae9
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-07-24 18:44:47 +0200

    chore(types): fix mapvalue and init (#1271)

--- langfuse/__init__.py
+++ langfuse/__init__.py
@@ -1,11 +1,10 @@
 """.. include:: ../README.md"""
 
-from ._client.client import Langfuse  # noqa
-from ._client.get_client import get_client  # noqa
-from ._client.observe import observe  # noqa
-from .version import __version__  # noqa
-from ._client.span import LangfuseSpan, LangfuseGeneration, LangfuseEvent
 from ._client.attributes import LangfuseOtelSpanAttributes
+from ._client.client import Langfuse
+from ._client.get_client import get_client
+from ._client.observe import observe
+from ._client.span import LangfuseEvent, LangfuseGeneration, LangfuseSpan
 
 __all__ = [
     "Langfuse",
