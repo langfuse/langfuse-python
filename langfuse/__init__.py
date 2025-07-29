eba5f7fed323c4e3eb00997a2746b69de38e58a5
Jan Winkler <45763961+janwinkler1@users.noreply.github.com>
2025-07-29 18:53:44 +0200

    fix(client): avoid private import of Langfuse to satisify Pyright (#1275)
    Co-authored-by: Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>

--- langfuse/__init__.py
+++ langfuse/__init__.py
@@ -1,10 +1,12 @@
 """.. include:: ../README.md"""
 
 from ._client.attributes import LangfuseOtelSpanAttributes
-from ._client.client import Langfuse
 from ._client.get_client import get_client
+from ._client import client as _client
 from ._client.observe import observe
 from ._client.span import LangfuseEvent, LangfuseGeneration, LangfuseSpan
+
+Langfuse = _client.Langfuse
 
 __all__ = [
     "Langfuse",
