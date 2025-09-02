abea0fa41ba68aa244d4dc1a26be50b618f021ef
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-09-02 12:48:11 +0200

    push

--- langfuse/langchain/CallbackHandler.py
+++ langfuse/langchain/CallbackHandler.py
@@ -939,7 +939,7 @@ class LangchainCallbackHandler(LangchainBaseCallbackHandler):
     def _convert_message_to_dict(self, message: BaseMessage) -> Dict[str, Any]:
         # assistant message
         if isinstance(message, HumanMessage):
-            message_dict = {"role": "user", "content": message.content}
+            message_dict: Dict[str, Any] = {"role": "user", "content": message.content}
         elif isinstance(message, AIMessage):
             message_dict = {"role": "assistant", "content": message.content}
 
@@ -949,6 +949,7 @@ class LangchainCallbackHandler(LangchainBaseCallbackHandler):
                 and len(message.tool_calls) > 0
             ):
                 message_dict["tool_calls"] = message.tool_calls
+
         elif isinstance(message, SystemMessage):
             message_dict = {"role": "system", "content": message.content}
         elif isinstance(message, ToolMessage):
