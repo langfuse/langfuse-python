e2d08ae48e66a7c8d35b19a7c25efca08a1d268f
Hassieb Pakzad <68423100+hassiebp@users.noreply.github.com>
2025-09-15 17:11:06 +0200

    push

--- langfuse/__init__.py
+++ langfuse/__init__.py
@@ -6,16 +6,16 @@ from ._client.constants import ObservationTypeLiteral
 from ._client.get_client import get_client
 from ._client.observe import observe
 from ._client.span import (
-    LangfuseEvent,
-    LangfuseGeneration,
-    LangfuseSpan,
     LangfuseAgent,
-    LangfuseTool,
     LangfuseChain,
     LangfuseEmbedding,
     LangfuseEvaluator,
-    LangfuseRetriever,
+    LangfuseEvent,
+    LangfuseGeneration,
     LangfuseGuardrail,
+    LangfuseRetriever,
+    LangfuseSpan,
+    LangfuseTool,
 )
 
 Langfuse = _client_module.Langfuse
@@ -36,4 +36,5 @@ __all__ = [
     "LangfuseEvaluator",
     "LangfuseRetriever",
     "LangfuseGuardrail",
+    "experiment",
 ]
