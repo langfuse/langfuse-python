71f443a8bca602e2165fc4baa2de5666aab7cb4f
Yashwanth <53632453+yash025@users.noreply.github.com>
2025-09-17 23:06:35 +0530

    feat(client): support custom export path to export spans (#1342)

--- langfuse/_client/span_processor.py
+++ langfuse/_client/span_processor.py
@@ -23,6 +23,7 @@ from langfuse._client.constants import LANGFUSE_TRACER_NAME
 from langfuse._client.environment_variables import (
     LANGFUSE_FLUSH_AT,
     LANGFUSE_FLUSH_INTERVAL,
+    LANGFUSE_OTEL_TRACES_EXPORT_PATH,
 )
 from langfuse._client.utils import span_formatter
 from langfuse.logger import langfuse_logger
@@ -90,8 +91,16 @@ class LangfuseSpanProcessor(BatchSpanProcessor):
         # Merge additional headers if provided
         headers = {**default_headers, **(additional_headers or {})}
 
+        traces_export_path = os.environ.get(LANGFUSE_OTEL_TRACES_EXPORT_PATH, None)
+
+        endpoint = (
+            f"{host}/{traces_export_path}"
+            if traces_export_path
+            else f"{host}/api/public/otel/v1/traces"
+        )
+
         langfuse_span_exporter = OTLPSpanExporter(
-            endpoint=f"{host}/api/public/otel/v1/traces",
+            endpoint=endpoint,
             headers=headers,
             timeout=timeout,
         )
