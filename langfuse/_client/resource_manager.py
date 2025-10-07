599a94bb3e6f6d716ba01a9058e500181be4ce8f
Thomas Wing <thomas.wing@dpdgroup.co.uk>
2025-10-07 10:48:59 +0100

    change headers_provider type hint in _langfuseInstrumentedHttpx

--- langfuse/_client/resource_manager.py
+++ langfuse/_client/resource_manager.py
@@ -464,7 +464,7 @@ class _LangfuseInstrumentedHttpx(httpx.Auth):
     def __init__(
         self,
         headers_provider: Optional[
-            Union[Mapping[str, Any], Callable[[], Mapping[str, Any]]]
+            Union[Mapping[str, str], Callable[[], Mapping[str, str]]]
         ],
     ):
         self._headers_provider = headers_provider
